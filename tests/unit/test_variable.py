50e445c4f21b9dd9993fc3caa7f9d9f5546acfa8
Andres Carrera <andreshcar@live.com>
2024-09-13 00:10:49 -0600

    Adding formatter tests

--- tests/unit/test_variable.py
+++ tests/unit/test_variable.py
@@ -1,4 +1,10 @@
-from templatr.formatter import DefaultFormatter, ListFormatter
+from typing import Any
+from templatr.formatter import (
+    DefaultFormatter,
+    ListFormatter,
+    VariableFormatter,
+    load_formatter,
+)
 
 
 def test_default_formatter__when_given_any_value__returns_value_as_is():
@@ -18,3 +24,28 @@ def test_list_formatter__when_given_list_with_single_item__returns_item_as_strin
     sut = ListFormatter(", ")
     value = sut.format([1])
     assert value == "1"
+
+
+def test_load_formatter__when_given_formatter_in_module__loads_formatter_correctly():
+    formatter = load_formatter("DefaultFormatter", [], {})
+    assert isinstance(formatter, DefaultFormatter)
+
+
+def test_load_formatter__when_given_formatter_with_args__loads_formatter_correctly():
+    formatter = load_formatter("ListFormatter", [", "], {})
+    assert formatter.seperator == ", "
+
+
+def test_load_formatter__when_given_formatter_with_kwargs__loads_formatter_correctly():
+    formatter = load_formatter("ListFormatter", [], {"seperator": ", "})
+    assert formatter.seperator == ", "
+
+
+class CustomFormatter(VariableFormatter):
+    def format(self, value: Any) -> Any:
+        return f"Custom: {value}"
+
+
+def test_load_formatter__when_given_custom_formatter__loads_formatter():
+    formatter = load_formatter("unit.test_variable.CustomFormatter", [], {})
+    assert isinstance(formatter, CustomFormatter)
